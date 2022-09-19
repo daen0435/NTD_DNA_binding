87f629c8ae66dea5add86809ee3d328bf470f428
David Matthew Engelhardt <daen0435@fiji-viz.colorado.edu>
2022-09-19 12:57:14 -0600

    added nextflow config and run files

---
+++ nf_core_chipseq_pipeline/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --job-name=NTD_chip
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=daen0435@colorado.edu 
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=10gb
+#SBATCH --time=120:00:00
+#SBATCH --output=nextflow.out
+#SBATCH --error=nextflow.err
+pwd; hostname; date
+echo "Lets go"
+module load singularity/3.1.1
+nextflow run nf-core/chipseq -r 1.2.1 \
+-profile singularity \
+--single_end \
+--input sdesign.csv \
+--fasta /scratch/Shares/public/genomes/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
+--gtf /scratch/Users/daen0435/NTDsurvey/NTD_DNA_binding/nf_core_chipseq_pipeline/gencode.vM10.annotation.gtf \
+--macs_gsize 2.7e9 \
+--blacklist /scratch/Users/daen0435/NTDsurvey/NTD_DNA_binding/nf_core_chipseq_pipeline/mm10-blacklist.v2.bed \
+--email daen0435@colorado.edu \
+-resume \
+-c nextflow.config
+date
